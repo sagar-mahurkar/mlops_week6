3112938828d81edb89e2973db24bc22fe63218e3
sagar-mahurkar <sagar.0.mahurkar@gmail.com>
2025-11-09 09:51:50 +0000

    Updated IP address

--- fetch_from_mlflow.py
+++ fetch_from_mlflow.py
@@ -3,7 +3,7 @@ import sys
 import mlflow
 from mlflow.tracking import MlflowClient
 
-URI = "http://34.31.11.39:5000/"
+URI = "http://34.45.3.75:5000/"
 NAME = "iris-random-forest"
 PATH = "downloaded_models"
 
